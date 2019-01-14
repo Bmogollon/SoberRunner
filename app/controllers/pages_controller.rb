f379ab39a64aa3917671c037e8fd73ffe1a0642f
Beerscheba chervil <designergo@aol.com>
2019-01-14 14:26:20 -0500

    login

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  before_action :authorise, only: [:welcome]
+
+  def welcome
+  end
+
+  
+
+  private
+    def authorise
+      redirect_to login_path unless (@current_user.present?)
+    end
+end
