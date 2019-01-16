688c757423f3bb3a2a2b6f060f1544a38f22aaec
Bonny Mogollon <bonny.mogollon11@gmail.com>
2019-01-16 16:21:45 -0500

    problem

---
+++ README.md
@@ -0,0 +1,12 @@
+# README
+
+Sober Runner Game.
+
+A PSA style game created using phaser and rails...
+
+Testing Tile maps.dd
+
+Startup Crunch
+
+Entering Soberrunner,, Ready to Add Warning Cards
+
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+
+  before_action :authorise, only: [:welcome]
+
+  def welcome
+
+  end
+
+
+
+  private
+    def authorise
+      redirect_to login_path unless (@current_user.present?)
+    end
+
+
+end
