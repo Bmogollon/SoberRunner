a886220b1950af9c84a23b04f3905db043334c87
Richard <richie1988@gmail.com>
2019-01-18 22:20:16 -0500

    removed insane animation on logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root :to => 'pages#welcome'
+  root 'pages#welcome'
   resources :users, :only => [:new, :create, :index, :update]
   get '/users/edit' => 'users#edit', :as => 'edit_user'
   post "/scores" => "scores#create"
