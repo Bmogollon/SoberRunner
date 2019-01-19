1c8800ba336429e20f1f62181d766784c06cf421
Richard <richie1988@gmail.com>
2019-01-18 22:09:55 -0500

    formatted routes page

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,14 @@
 Rails.application.routes.draw do
-
   root :to => 'pages#welcome'
-   resources :users, :only => [:new, :create, :index, :update]
-   get '/users/edit' => 'users#edit', :as => 'edit_user'
-
-   post "/scores" => "scores#create"
-   get "/scores" => "scores#index"
-   get '/signup' => 'users#new'
-
-   get '/login' => 'session#new'
-   post '/login' => 'session#create'
-   delete '/login' => 'session#destroy'
-
-
-   get '/user' => 'user#new'
-   get "games/index"
-
-
+  resources :users, :only => [:new, :create, :index, :update]
+  get '/users/edit' => 'users#edit', :as => 'edit_user'
+  post "/scores" => "scores#create"
+  get "/scores" => "scores#index"
+  get '/signup' => 'users#new'
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
+  get '/user' => 'user#new'
+  get "games/index"
   resources :poppers
 end
